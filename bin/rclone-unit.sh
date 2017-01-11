cad836df774bbb70993e8d24280e8e25ffe2be8f
Michał Sawicz <michal@sawicz.net>
2017-01-11 18:11:38 +0100

    Move the smarts out of the unit file
    Trying to be too smart inside the unit file just makes things worse.

--- bin/rclone-unit.sh
+++ bin/rclone-unit.sh
@@ -4,18 +4,21 @@ set -e
 
 usage() {
   echo "Usage:"
-  echo "  $0 command src dest"
+  echo "  $0 command item"
   echo
   echo "  command: oper-name, where oper is one of copy, sync, move"
+  echo "  item:    name of the subdirectory to process"
   echo
   echo "  The following environment variables are read, in order of precedence:"
+  echo "    name_SRC:"
+  echo "    name_DEST:      rclone source and destination paths"
   echo "    name_EXCLUDE:"
   echo "    RCLONE_EXCLUDE: passed as --exclude-from, if exists under src/ and/or dest/"
   echo "    name_OPTIONS:"
   echo "    RCLONE_OPTIONS: both passed to rclone as-is"
 }
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 2 ]; then
   usage
   exit 2
 fi
@@ -30,17 +33,19 @@ else
   exit 1
 fi
 
+src_varname="${name}_SRC"
+dest_varname="${name}_DEST"
 exclude_varname="${name}_EXCLUDE"
 options_varname="${name}_OPTIONS"
 
 exclude=${!exclude_varname-${RCLONE_EXCLUDE-".rclone.exclude"}}
 
-src="$2"
-dest="$3"
+item="$2"
+src="${!src_varname}/${item}"
+dest="${!dest_varname}/${item}"
+
 read -a opts <<< "${RCLONE_OPTIONS} ${!options_varname}"
 opts+=("--exclude=${exclude}")
-
-item=$( echo ${src} | awk -F/ '{ print $(NF) }' )
 
 echo "========== ${item} =========="
 
